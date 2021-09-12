29a4e6ba221b8319f902260c4149ed60c4fcffa5
Carlos Duarte <carlosf.duarte8@gmail.com>
2021-09-12 08:58:19 -0300

    Api

--- src/services/Api/index.tsx
+++ src/services/Api/index.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:7000'
+  baseURL: 'http://192.168.0.105:7000'
 });
+console.log('API ==>', api);
 
 export default api;
