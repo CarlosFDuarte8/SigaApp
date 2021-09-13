5ae84caeafd8d789c9b166b14d7c22b1a21addf4
Carlos Duarte <carlosf.duarte8@gmail.com>
2021-09-13 18:15:04 -0300

    implementação Searchbar

--- src/screens/Setting/Users.tsx
+++ src/screens/Setting/Users.tsx
@@ -1,6 +1,6 @@
 import React, { Component, useState, useEffect } from 'react';
 import { Alert, TextInput, View, Text, Image, TouchableOpacity, } from 'react-native';
-import { DataTable, Switch, FAB } from 'react-native-paper';
+import { DataTable, Switch, FAB, Searchbar } from 'react-native-paper';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import { api } from '../../services';
@@ -19,6 +19,7 @@ interface UserTitleProps {
   name: string;
   login: string;
   password: string;
+  profile: string;
   situation: boolean;
 }
 
@@ -28,13 +29,14 @@ const UserTitles: UserTitleProps[] = [
     name: "Nome",
     login: "Usuário",
     password: "Senha",
+    profile: "Perfil",
     situation: "Status"
   }
 ]
 
-const optionsPerPage = [2, 3, 4];
+const optionsPerPage = [2, 3];
 
-const ListUsers = ({navigation}) => {
+const ListUsers = ({ navigation }) => {
   const [users, setUsers] = useState<UserProps[]>([]);
 
   useEffect(() => {
@@ -46,7 +48,11 @@ const ListUsers = ({navigation}) => {
   console.clear();
 
   const [page, setPage] = React.useState<number>(0);
-  const [itemsPerPage, setItemsPerPage] = React.useState(optionsPerPage[2]);
+  const [itemsPerPage, setItemsPerPage] = React.useState(optionsPerPage[0]);
+
+  const [searchQuery, setSearchQuery] = React.useState('');
+
+  const onChangeSearch = query => setSearchQuery(query);
 
   const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
 
@@ -57,63 +63,72 @@ const ListUsers = ({navigation}) => {
   return (
     <View style={listUsers.container} >
       <Header
-       navigation={navigation} 
-      />
+        navigation={navigation}
+      /><View style={listUsers.containers} >
 
-    <DataTable style={{ flex: 1.5, marginTop: 0 }}>
-      {
-        UserTitles.map(({ id, name, login, password, situation }: menuProps) => {
-          return (
-            <DataTable.Header key={id}>
-              <DataTable.Title>{id}</DataTable.Title>
-              <DataTable.Title>{name}</DataTable.Title>
-              <DataTable.Title>{login}</DataTable.Title>
-              <DataTable.Title>{password}</DataTable.Title>
-              <DataTable.Title>{situation}</DataTable.Title>
-            </DataTable.Header>
-          );
-        })
-      }
-      {
-        users.map(({ id, name, login, password, situation }: UserProps) => {
-          return (
-            <DataTable.Row key={id}>
-              <DataTable.Cell>{id}</DataTable.Cell>
-              <DataTable.Cell>{name}</DataTable.Cell>
-              <DataTable.Cell>{login}</DataTable.Cell>
-              <DataTable.Cell>{password}</DataTable.Cell>
-              <DataTable.Cell>
-                {/* <FAB
-                  style={{
-                    position: 'absolute',
-                    margin: 6,
-                    right: 2,
-                    bottom: 0,
-                  }}
-                  small
-                  icon="plus"
-                // onPress={opts}
-                /> */}
-                <Switch value={situation} onValueChange={onToggleSwitch} />
-              </DataTable.Cell>
-              {/* <DataTable.Cell numeric>6.0</DataTable.Cell> */}
-            </DataTable.Row>
-          );
-        })
-      }
-      <DataTable.Pagination
-        page={page}
-        numberOfPages={3}
-        onPageChange={(page) => setPage(page)}
-        label="1-2 of 6"
-        optionsPerPage={optionsPerPage}
-        itemsPerPage={itemsPerPage}
-        setItemsPerPage={setItemsPerPage}
-        showFastPagination
-        optionsLabel={'Rows per page'}
+      <Searchbar
+        placeholder="Search"
+        onChangeText={onChangeSearch}
+        value={searchQuery}
       />
-    </DataTable>
-    <Footer navigation={navigation} />
+      <DataTable style={{ flex: 1, marginTop: 0,  }}>
+        {
+          UserTitles.map(({ id, name, login, password, situation, profile }: menuProps) => {
+            return (
+              <DataTable.Header key={id}>
+                <DataTable.Title >{id}</DataTable.Title>
+                <DataTable.Title>{name}</DataTable.Title>
+                <DataTable.Title>{login}</DataTable.Title>
+                <DataTable.Title>{profile}</DataTable.Title>
+                <DataTable.Title >{situation}</DataTable.Title>
+                <DataTable.Title >Opções</DataTable.Title>
+              </DataTable.Header>
+            );
+          })
+        }
+        {
+          users.map(({ id, name, login, password, situation, profile }: UserProps) => {
+            const Ativo = situation;
+            return (
+              <DataTable.Row key={id}>
+                <DataTable.Cell centered={true}>{id}</DataTable.Cell>
+                <DataTable.Cell>{name}</DataTable.Cell>
+                <DataTable.Cell>{login}</DataTable.Cell>
+                <DataTable.Cell>{profile}</DataTable.Cell>
+                <DataTable.Cell disabled>
+                  <Switch value={situation} onValueChange={onToggleSwitch} />
+                </DataTable.Cell>
+                <DataTable.Cell >
+                  <FAB
+                    style={{
+                      position: 'absolute',
+                      margin: 6,
+                      right: 2,
+                      bottom: 0,
+                    }}
+                    small
+                    icon="plus"
+                  // onPress={opts}
+                  />
+                </DataTable.Cell>
+              </DataTable.Row>
+            );
+          })
+        }
+        <DataTable.Pagination
+          page={page}
+          numberOfPages={3}
+          onPageChange={(page) => setPage(page)}
+          label="1-1 of 1"
+          optionsPerPage={optionsPerPage}
+          itemsPerPage={itemsPerPage}
+          setItemsPerPage={setItemsPerPage}
+          showFastPagination
+          optionsLabel={'Rows per page'}
+        />
+      </DataTable>
+      </View>
+      <Footer navigation={navigation} />
     </View>
   );
 
