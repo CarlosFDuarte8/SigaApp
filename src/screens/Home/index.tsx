2d70894602f28c73fffe2725dd30433a041fc157
Carlos Duarte <carlosf.duarte8@gmail.com>
2021-09-22 12:06:51 -0300

    :construction: New home in construction

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,43 +1,87 @@
 import React from 'react';
-import { Button, Text, View, TouchableOpacity } from 'react-native';
+import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar, TouchableOpacity, Dimensions } from 'react-native';
+import { Divider } from 'react-native-paper';
 import { StackNavigationProp } from '@react-navigation/stack';
-import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { FontAwesome, Feather, MaterialCommunityIcons } from '@expo/vector-icons';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
-import { home } from '../../globalStyles/styles';
 
-interface menuProps {
-  id: number,
-  icon: string,
+interface homeItens {
+  id: string,
   title: string,
-  path: string,
-  libIcon: any,
+  subTi: string,
+  iconn: string
 }
 
-const menus: menuProps[] = [
+const DATA = [
   {
-    id: 1,
-    icon: 'cash-multiple',
-    title: 'Tesouraria',
+    id: '1',
+    title: 'Ambiente de aprendizagem',
+    subTi: 'Tutorias, Manuais...',
+    iconn: 'school',
+    path: 'Learnings',
+  },
+  {
+    id: '2',
+    title: 'Tesoraria',
+    subTi: 'Deposito, Fechamento, Relatórios...',
+    iconn: 'cash-multiple',
     path: 'Cash',
-    libIcon: <MaterialCommunityIcons name='cash-multiple' color={'#033d60'} size={35} />,
   },
   {
-    id: 2,
-    icon: 'account-group',
-    title: 'Voluntário',
-    path: 'Setting',
-    libIcon: <MaterialCommunityIcons name="account-group" color={'#033d60'} size={35} />,
+    id: '3',
+    title: 'Mês de trabalho',
+    subTi: 'Setembro de 2021',
+    iconn: 'calendar',
+    path: 'Works',
   },
-  { 
-    id: 3,
-    icon: 'package-variant',
-    title: 'Produtos/Matérias',
+  {
+    id: '4',
+    title: 'Vonluntários',
+    subTi: 'Apontamentos & Cadastro',
+    iconn: 'account-group-outline',
+    path: 'Volunteers',
+  },
+  {
+    id: '5',
+    title: 'Materias & Produtos',
+    subTi: 'Estoque & Relatórios',
+    iconn: 'package-variant',
     path: 'Product',
-    libIcon: <MaterialCommunityIcons name="package-variant" color={'#033d60'} size={35} />,
+  },
+  {
+    id: '6',
+    title: 'Suporte',
+    subTi: 'Iniciar um novo chat',
+    iconn: 'chat-plus-outline',
+    path: 'Suport',
+  },
+  {
+    id: '7',
+    title: 'Informações',
+    subTi: 'Versão do app...',
+    iconn: 'information-outline',
+    path: 'Info',
   },
 ];
+
+const Item = ({ title, iconn, subTi, path }) => (
+  <>
+  <View style={styles.item}>
+    <TouchableOpacity style={styles.buttonTitle} onPress={() => console.log('Press texto', path)}>
+      <Text style={styles.title}>  {title}</Text>
+      <Text style={styles.subTitle}>  {subTi}</Text>
+    <TouchableOpacity style={styles.iconS} onPress={() => console.log('Press icon', path)}>
+      <MaterialCommunityIcons name={iconn} size={54} color="#033d60" />
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.dots} onPress={() => console.log('Press arrow', path)}>
+      <MaterialCommunityIcons name="chevron-right" size={34} color="#033d60" />
+    </TouchableOpacity>
+    </TouchableOpacity>
+  </View>
+    <Divider/>
+    </>
+);
 
 type TopNavProp = {
   Login: undefined;
@@ -45,57 +89,85 @@ type TopNavProp = {
 };
 
 interface Props {
-  navigation: StackNavigationProp<TopNavProp, 'Login'>
+  navigation: StackNavigationProp<TopNavProp, 'Setting'>
 }
 
 const Home = ({ navigation }: Props) => {
+  const renderItem = ({ item }) => (
+    <Item title={item.title} iconn={item.iconn} subTi={item.subTi} path={item.path}/>
+  );
+
   return (
-    <View style={home.container}>
+    <>
       <Header
         navigation={navigation}
       />
-      {
-        menus.map(({id, icon, title, path, libIcon }: menuProps) => {
-          return (
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+        >
 
-            <View style={home.body}>
-
-              <View key={id}
-              // style={home.subBody}
-              >
-
-                <TouchableOpacity
-                  style={home.buttonGrid}
-                  // onPress={() => navigation.navigate('Login')}
-                  onPress={() => navigation.navigate(path)}
-                >
-                  <View style={home.buttonIcon}>
-                    <MaterialCommunityIcons name={icon} style={home.icon} />
-                  </View>
-                  <View style={home.buttonTitle}>
-
-                    <Text style={home.titleGrid}>
-                      {title}
-                    </Text>
-                  </View>
-                </TouchableOpacity>
-
-              </View>
-
-            </View>
-          );
-        })
-      }
-
-      <View style={{ marginTop: 100, }}>
-
-        <Footer
-          navigation={navigation}
-        />
-      </View>
-    </View>
+        </FlatList>
+      </SafeAreaView>
+      <Footer
+        navigation={navigation}
+      />
+    </>
   );
-};
+}
 
+const { width, height } = Dimensions.get("screen");
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#fff',
+    padding: height * 0.05,
+    marginVertical: 0,
+    marginHorizontal:0,
+    borderRadius: 5,
+    flexDirection: 'row'
+  },
+  buttonTitle: {
+    flexDirection: "column",
+    position: "absolute",
+    width: width * 0.925,
+    height: height * 0.099,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    // borderColor: "red",
+    // borderStyle: 'solid',
+    // borderWidth: 1,
+  },
+  title: {
+    fontSize: width * 0.05,
+    color: "#033d60",
+    marginHorizontal: width * 0.012,
+    // position: "absolute",
+  },
+  subTitle: {
+    fontSize: width * 0.04,
+    color: "#033d60",
+    marginHorizontal: width * 0.22,
+  },
+  buttons: {
+    position: "absolute",
+    marginHorizontal: width * 0.05,
+  },
+  iconS: {
+    position: "absolute",
+    marginHorizontal: width * 0.05,
+    marginVertical: height * 0.03,
+  },
+  dots: {
+    position: "absolute",
+    marginHorizontal: width * 0.8,
+    marginVertical: height * 0.026,
+  }
+});
 
 export default Home;
